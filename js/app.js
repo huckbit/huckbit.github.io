87c17ef2df77d4754ca3de7740a0bc61652b56a1
hukbit <huckbit@gmail.com>
2016-08-31 15:05:03 +0100

    disquis script configuration updated

--- js/app.js
+++ js/app.js
@@ -13,11 +13,11 @@
   ----------------------------------------------------------------------
 */
 
-// /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-// var disqus_shortname = 'huckbit'; // required: replace example with your forum shortname
-// /* * * DON'T EDIT BELOW THIS LINE * * */
-// (function() {
-//     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-//     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-//     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-// })();
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'huckbit'; // required: replace example with your forum shortname
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
