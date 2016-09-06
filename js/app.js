8b04c615ef4c7eb2f03b66ee46800c4fd348ac19
Massimiliano Ranauro <huckbit@gmail.com>
2016-09-06 18:27:43 +0100

    added goTop button

--- js/app.js
+++ js/app.js
@@ -14,21 +14,37 @@
 * Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/
 * Description: Performs a smooth page scroll to an anchor on the same page.
 */
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 500);
 
-      return false;
+(function(){
+  /* The function shows the back-to-top icon when scrolls more than 50px */
+  var icon = $('.go-up');
+  icon.hide();
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 500) {
+      icon.fadeIn('ease-in');
+    } else {
+      icon.fadeOut('ease-out');
     }
-  }
-});
-});
+  });
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+          scrollTop: target.offset().top
+          }, 500);
+
+          return false;
+        }
+      }
+    });
+  });
+
+}());
 
 
 
