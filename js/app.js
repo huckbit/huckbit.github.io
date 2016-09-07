d711064fc18e122f2e9b37c42dd4d6de0e562d5b
Massimiliano Ranauro <huckbit@gmail.com>
2016-09-07 14:44:17 +0100

    some changes on style

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@
 */
 
 (function(){
-  /* The function shows the back-to-top icon when scrolls more than 50px */
+  /* The function shows the back-to-top icon when scrolls more than 500px */
   var icon = $('.go-up');
   icon.hide();
 
@@ -28,20 +28,19 @@
     }
   });
 
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-          scrollTop: target.offset().top
-          }, 500);
+  //smooth Scrolling
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+        scrollTop: target.offset().top
+        }, 500);
 
-          return false;
-        }
+        return false;
       }
-    });
+    }
   });
 
 }());
